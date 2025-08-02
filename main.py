b2e08ef3791e44b0c811fdde5435182b5b190f7c
Gabriel GoZe <gabrielbgz.14@gmail.com>
2025-08-02 11:03:29 -0500

    Change design of app

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     # --- Setup the main window ---
     root = tk.Tk()
     root.title("Audio Metadata Editor")
-    root.geometry("500x600")
+    root.geometry("600x720")
 
     # --- Create the UI ---
     # This creates an instance of our AppUI class,
--- ui_app.py
+++ ui_app.py
@@ -6,76 +6,83 @@ from function_app import AppFunctions
 class AppUI:
     def __init__(self, root):
         self.root = root
+        self.root.configure(background='gainsboro')
         # Create an instance of the functions class
         self.functions = AppFunctions(self.root)
 
         # --- UI Elements ---
-        label_title = tk.Label(root, text="Edit Audio Metadata",
-                               font=('Arial', 34))
-        label_title.pack(pady=20)
+
+        label_title = tk.Label(
+            root,
+            text="Edit Audio Metadata",
+            font=('Arial bold', 34, 'bold'),
+            bg='gainsboro'
+        )
+        label_title.pack(pady=20, padx=50, anchor='w')
 
         button_select_song = tk.Button(
             root, font='12',
             text="Seleccionar canción MP3",
-            command=self.on_select_mp3)
-
-        button_select_song.pack(pady=5, padx=22, anchor='w')
+            command=self.on_select_mp3
+        )
+        button_select_song.pack(pady=5, padx=50, anchor='w')
 
         self.file_name = tk.Label(
             root,
             text='Nombre de archivo',
-            font='14').pack()
+            font='14',
+            bg='gainsboro'
+        ).pack(padx=50, anchor='w')
 
-        self.label_mp3 = tk.Label(
-            root, text="Archivo no seleccionado", font='12')
-        self.label_mp3.pack()
+        self.entry_file_name = tk.Entry(root, width=50)
+        self.entry_file_name.pack(padx=50, anchor='w')
 
-        self.entry_file_name = tk.Entry(root, width=30)
-        self.entry_file_name.pack()
+        tk.Label(root, text="Título", font='14',
+                 bg='gainsboro').pack(padx=50, anchor='w')
 
-        tk.Label(root, text="Título", font='14').pack()
+        self.entry_titulo = tk.Entry(root, width=50)
+        self.entry_titulo.pack(padx=50, anchor='w')
 
-        self.entry_titulo = tk.Entry(root, width=30)
-        self.entry_titulo.pack()
+        tk.Label(root, text="Artista", font='14',
+                 bg='gainsboro').pack(padx=50, anchor='w')
 
-        tk.Label(root, text="Artista", font='14').pack()
+        self.entry_artista = tk.Entry(root, width=50)
+        self.entry_artista.pack(padx=50, anchor='w')
 
-        self.entry_artista = tk.Entry(root, width=30)
-        self.entry_artista.pack()
+        tk.Label(root, text="Álbum", font='14',
+                 bg='gainsboro').pack(padx=50, anchor='w')
 
-        tk.Label(root, text="Álbum", font='14').pack()
-
-        self.entry_album = tk.Entry(root, width=30)
-        self.entry_album.pack()
+        self.entry_album = tk.Entry(root, width=50)
+        self.entry_album.pack(padx=50, anchor='w')
 
         tk.Button(
             root,
             text="Seleccionar portada (JPG)",
             font='14',
-            command=self.select_cover_album).pack(pady=5)
+            command=self.select_cover_album).pack(pady=10, padx=50, anchor='w')
 
         self.label_portada = tk.Label(
             root,
             text="Imagen no seleccionada",
-            font='12')
-        self.label_portada.pack()
+            font='12',
+            bg='gainsboro'
+        )
+        self.label_portada.pack(padx=50, anchor='w')
 
         tk.Button(root,
                   text='Guardar medatata',
-                  command=self.save).pack()
+                  command=self.save).pack(padx=50, anchor='w')
+
+    # --- Functions to buttons ---
 
     def on_select_mp3(self):
         mp3_file = self.functions.select_mp3_file()
         if mp3_file:
-            self.label_mp3.config(text=mp3_file)
             self.entry_file_name.insert(0, mp3_file)
             self.entry_titulo.insert(0, os.path.basename(mp3_file))
 
     def select_cover_album(self):
         path_cover, label_cover = self.functions.select_cover_album()
-        print('--- ABOUT COVER ---')
-        # print('path_cover'+path_cover)
-        # print('label_cover'+label_cover)
 
     def save(self):
         self.functions.save_metadata(
@@ -94,4 +101,3 @@ class AppUI:
         self.entry_titulo.delete(0, tk.END)
         self.entry_artista.delete(0, tk.END)
         self.entry_album.delete(0, tk.END)
-        self.label_mp3.config(text='Archivo no seleccionado', font=14)
